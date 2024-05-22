c5b79aabeadb056578c0096e538cbccde7cc8a22
PFGrande <2002.pedrogrande@gmail.com>
2024-05-21 23:45:03 -0700

    added comments

--- steering.c
+++ steering.c
@@ -3,6 +3,7 @@
  * 
  * Group Member Names:: Tyler Hsieh, Anthony Silva, Pedro Grande, Rafael Sant Ana Leitao
  * SFSU IDs: 920216320, 922907645, 921149265, 922965105
+ * Github Usernames: Tyler9648, anthonySilva23, PFGrande, Rafael-Leitao
  * 
  * Project:: Autonomous Line-Following Car
  *
@@ -48,8 +49,13 @@ void terminateSteering()
     Motor_Terminate();
 }
 
+// check if there is an obstacle within 20 cm of the sonar
 int obsDetected(void){
-    if ((sonarSensorArgs->value <= DETECT_OBS_DIST && sonarSensorArgs->value > 0) || avoidSensorArgs->value == 0){   //if obstacle detected
+    //if obstacle detected
+    if ((sonarSensorArgs->value <= DETECT_OBS_DIST && 
+        sonarSensorArgs->value > 0) || avoidSensorArgs->value == 0
+        ) 
+    {   
         return 1;
     } else {
         return 0;
@@ -57,6 +63,7 @@ int obsDetected(void){
    
 }
 
+// Default behavior that allows the car to follow the line
 int steer()
 {
     Motor_setVelocity(MOTORA, BASE_SPEED);
@@ -72,36 +79,51 @@ int steer()
     }
 
     // for sharp turns, slow down the speed
-    if (multiLineSensorArgs->value3 == 1 && (multiLineSensorArgs->value1 == 1 || multiLineSensorArgs->value5 == 1) && (multiLineSensorArgs->value1 != multiLineSensorArgs->value5))
+    if (
+         multiLineSensorArgs->value3 == 1 && 
+        (multiLineSensorArgs->value1 == 1 || multiLineSensorArgs->value5 == 1) && 
+        (multiLineSensorArgs->value1 != multiLineSensorArgs->value5)
+        )
     {
         Motor_setVelocity(MOTORA, BASE_SPEED / 2);
         Motor_setVelocity(MOTORB, BASE_SPEED / 2);
         sleepVal *= 0.65;
     }
     int steerValue = getSum();
-    while (exitThread == 0 && /*getSum()*/ steerValue != 0 && obsDetected() == 0)
-    { // while its not an intersection or not dead center on the line
+
+    // while its not an intersection or not dead center on the line, 
+    // correct the position of the car using the value from getSum()
+    while (exitThread == 0 && steerValue != 0 && obsDetected() == 0)
+    {
         steerValue = getSum();
         
+        // currSpeed is in the range of -100 to 100
+        // currSpeed is updated by Motor_setVelocity() 
         if((motorASpeed + steerValue) > MIN_TURNING_SPEED)
-            Motor_setVelocity(MOTORA, motorASpeed + steerValue); // assume currSpeed is updated by setVelocity, and is between -100 to 100
-        if((motorBSpeed - steerValue) > MIN_TURNING_SPEED)
-            Motor_setVelocity(MOTORB, motorBSpeed - steerValue); // this doesn't require encoder cus im lazy, but if needed can be changed
+            Motor_setVelocity(MOTORA, motorASpeed + steerValue); 
+        if((motorBSpeed - steerValue) > MIN_TURNING_SPEED) 
+            Motor_setVelocity(MOTORB, motorBSpeed - steerValue); // Function does not use encoder
         usleep(sleepVal);
     }
 
     return 0;
 }
 
+// behavior of car after an obstacle is detected
 void avoidObstacle()
 {
+    // To prevent motors from frying, they are stopped before changing
+    // direction
 
-    // Stop
+    // Stop car
     Motor_setVelocity(MOTORA, 0);
     Motor_setVelocity(MOTORB, 0);
     usleep(1000000);
+
+    // point sonar at the obstacle
     Pan_Right();
-    // Turn car left, and sonar towards the obstacle
+
+    // Steer car left
     Motor_setVelocity(MOTORA, -40);
     Motor_setVelocity(MOTORB, 40);
     usleep(1000000);
@@ -110,38 +132,48 @@ void avoidObstacle()
     Motor_setVelocity(MOTORB, 0);
     usleep(1000000);
 
+    // go around an object at a set speed, not the speed at which the car
+    // was going at on the line
     Motor_setVelocity(MOTORA, AVOIDANCE_SPEED);
     Motor_setVelocity(MOTORB, AVOIDANCE_SPEED);
     
-    //  Keep going around the obstacle until we find the line or Ctr + C
+    // Keep going around the obstacle until we find the line or Ctr + C
+    // maintain a set distance between the car and the obstacle
+    // to allow the car to circle around it
     while (multiLineSensorArgs->value3 != 1 && exitThread == 0)
     {
         printf("sonar distance: %d\n", sonarSensorArgs->value);
-        int motorAadjust = 1;                                 //how much to adjust motor speed by 
+        int motorAadjust = 1; //how much to adjust motor speed by 
         int motorBadjust = 1;
 
-        if (sonarSensorArgs->value < MIN_OBS_DIST)            //we're too close to obstacle, so turn away
-        {
-            if((motorASpeed - 1) <= MIN_OBS_AVOID_SPEED){       //dont want to go too slow
+        //we're too close to obstacle, so turn away
+        if (sonarSensorArgs->value < MIN_OBS_DIST) 
+        {   
+            // dont want to go too slow
+            if((motorASpeed - 1) <= MIN_OBS_AVOID_SPEED){ 
                 motorAadjust = 0;
             }
-            if((motorBSpeed + 1) >= MAX_OBS_AVOID_SPEED){       //also dont want to go too fast
+
+            //also dont want to go too fast
+            if((motorBSpeed + 1) >= MAX_OBS_AVOID_SPEED){ 
                 motorBadjust = 0;
             }
-            if(sonarSensorArgs->value < MIN_OBS_DIST * 0.7){        //IF IT IS REALLY WAY TOO CLOSE TO THE OBSTACLE
-                motorAadjust *= 2;                                  //THEN ADJUST EVEN MORE
+
+            //IF IT IS REALLY WAY TOO CLOSE TO THE OBSTACLE
+            if(sonarSensorArgs->value < MIN_OBS_DIST * 0.7){ 
+                motorAadjust *= 2; //THEN ADJUST EVEN MORE
                 motorBadjust *= 2;
             }
             Motor_setVelocity(MOTORA, motorASpeed - motorAadjust);
             Motor_setVelocity(MOTORB, motorBSpeed + motorBadjust);
         }
-        else if (                                              //optimal distance, so go straight
+        else if ( //optimal distance, so go straight
             (sonarSensorArgs->value < MAX_OBS_DIST))
             {
                 Motor_setVelocity(MOTORA, AVOIDANCE_SPEED);
                 Motor_setVelocity(MOTORB, AVOIDANCE_SPEED);
             }
-        else                                                   //were too far from obstacle, so turn towards it 
+        else //were too far from obstacle, so turn towards it 
         {
             if((motorASpeed + 1) >= MAX_OBS_AVOID_SPEED){
                 motorAadjust = 0;
@@ -149,17 +181,21 @@ void avoidObstacle()
             if((motorBSpeed - 1) <= MIN_OBS_AVOID_SPEED){
                 motorBadjust = 0;
             }
+            // adjust the motors based on distance from object
             Motor_setVelocity(MOTORA, motorASpeed + motorAadjust);
             Motor_setVelocity(MOTORB, motorBSpeed - motorBadjust);
         }
-        usleep(OBS_FREQ_DELAY);      
+        usleep(OBS_FREQ_DELAY);     
     }
+    // set servo to face forward
     Pan_Forward();
     printf("\nwere back on the line\n");
+
+    // Return to the line after done circling around obstacle
     Motor_setVelocity(MOTORA, -30);
     Motor_setVelocity(MOTORB, 30);
     usleep(1000000);
-     Motor_setVelocity(MOTORA, 0);
+    Motor_setVelocity(MOTORA, 0);
     Motor_setVelocity(MOTORB, 0);
     usleep(1000);
     Motor_setVelocity(MOTORA, AVOIDANCE_SPEED);
@@ -167,57 +203,14 @@ void avoidObstacle()
     prevSum = 2;
 }
 
-void testAvoidObstacle()
-{
-
-    // Stop
-    Motor_setVelocity(MOTORA, 0);
-    Motor_setVelocity(MOTORB, 0);
-    usleep(1000000);
-    Pan_Right();
-    // Turn left
-
-    //Motor_setVelocity(MOTORA, -40);
-    Motor_setVelocity(MOTORB, 40);
-    //usleep(1000000);
-    usleep(2000000);
-
-    Motor_setVelocity(MOTORA, 0);
-    Motor_setVelocity(MOTORB, 0);
-    usleep(1000000);
-
-    Motor_setVelocity(MOTORA, AVOIDANCE_SPEED);
-    Motor_setVelocity(MOTORB, AVOIDANCE_SPEED);
-    // loop aroun in circle intil
-    //  Move Forward
-    while (lineSensorThreeArgs->value != 1 && exitThread == 0)
-    {
-        printf("sonar distance: %d\n", sonarSensorArgs->value);
-        if (sonarSensorArgs->value >= 20 && sonarSensorArgs->value <= 38){
-            Motor_setVelocity(MOTORA, AVOIDANCE_SPEED);
-            Motor_setVelocity(MOTORB, AVOIDANCE_SPEED);
-        }
-        else {
-        double adjust = (sonarSensorArgs->value - 25) * pow( 1.05, abs(30 - sonarSensorArgs->value)/12 );
-        printf("adjsut val: %f\n", adjust);
-        if (adjust < 0){
-            adjust *= 3;
-        }
-        Motor_setVelocity(MOTORA, motorASpeed + (int) (adjust * 0.02));
-        Motor_setVelocity(MOTORB, motorBSpeed - (int) (adjust * 0.02));
-        }
-        usleep(150000);
-    }
-    Pan_Forward();                              //cat: w3e0p-2p
-    printf("\nwere back on the line\n");
-}
-
+// Each sensor's assigned weight and sign determine the direction and speed 
+// of the car
 int getSum()
 {
-
-    // start with outter sensors?
     double sum = 0;
     int sensorsTriggered = 0;
+
+    // check return value for all line sensors
     if (multiLineSensorArgs->value1 == 1)
     {
         sum += 3;
@@ -243,15 +236,23 @@ int getSum()
         sensorsTriggered++;
     }
 
+    // When the car is off the line, return the direction the
+    // car was going in befor leaving the line
     if (sensorsTriggered == 0)
     {
         return prevSum;
     }
 
     int intSum;
-    if (multiLineSensorArgs->value3 == 1 && (multiLineSensorArgs->value1 == 1 || multiLineSensorArgs->value5 == 1) && (multiLineSensorArgs->value1 != multiLineSensorArgs->value5))
+
+    // Adjust turn speed when encountering sharp turns with the outer sensors and
+    // go through intersections
+    if  (multiLineSensorArgs->value3 == 1 && 
+        (multiLineSensorArgs->value1 == 1 || multiLineSensorArgs->value5 == 1) && 
+        (multiLineSensorArgs->value1 != multiLineSensorArgs->value5)
+        )
     {
-        intSum = (int)(sum / (sensorsTriggered - 1)); // this is really scuffed but it works
+        intSum = (int)(sum / (sensorsTriggered - 1));
         intSum = (intSum / abs(intSum)) * 4;          // 4 is total sum of one side of sensors
     }
     else
@@ -259,6 +260,8 @@ int getSum()
         intSum = (int)(sum / sensorsTriggered); // it is totally intentional to have it 0.5 be rounded down to 0
     }                                           // when converting double to int, for averaging sum of 1 between
                                                 // a middle and inner line sensor
+
+    // update the previous sum while the car is on the line
     if (intSum != 0)
     {
         prevSum = intSum;
